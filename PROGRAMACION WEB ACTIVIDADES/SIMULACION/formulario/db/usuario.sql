d664eb7c982ae304cbe9e54e7a19c20b0bfe84ad
jorgecuxim <122658803+jorgecuxim@users.noreply.github.com>
2023-08-19 23:02:36 -0600

    Delete PROGRAMACION WEB ACTIVIDADES directory

--- PROGRAMACION WEB ACTIVIDADES/SIMULACION/formulario/db/usuario.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE usuario( 
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY, 
-    nombre VARCHAR(150), 
-    email VARCHAR(50), 
-    tel VARCHAR(10) 
-);
--- PROGRAMACION WEB ACTIVIDADES/SIMULACION/formulario/while.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $repeticiones = 5;
-
-    while($a <= $repeticiones){
-        echo " a vale {$a}\n";
-        $a++;
-    }
-?>
